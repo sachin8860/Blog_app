0b024db2cc6e9e96bdf16f3f02c29cfa0d2093b2
w3villa-sachin <sachin.srivastava@w3villa.com>
2022-07-06 17:01:34 +0530

    tags added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,7 +5,7 @@ class Blog < ApplicationRecord
     has_one_attached :image
     belongs_to :category
 
-    has_many :blog_tags
+    has_many :blog_tags, dependent: :destroy
     has_many :tags, through: :blog_tags
 
     accepts_nested_attributes_for:blog_tags
