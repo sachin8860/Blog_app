aa8b264a3a6d65c827e1157a2fb909af646692d9
W3villa-Sachin <sachin.srivastava@w3villa.com>
2022-07-09 16:29:51 +0530

    deleted unwanted migrations

--- db/migrate/20220704084607_add_column.rb
+++
@@ -1,6 +0,0 @@
-class AddColumn < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :blog_tags, :blog, foreing_key: true
-    add_reference :blog_tags, :tag, foreing_key: true
-  end
-end
